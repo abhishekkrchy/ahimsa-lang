0435732fb58365f307738792bd394635fc441eef
Abhishek Kumar <abhishek.kr.chy@gmail.com>
2019-12-02 10:03:46 +0530

    added install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+./gradlew clean install
+sudo mkdir -p /usr/local/ahimsa/
+sudo cp build/libs/ahimsa-lang-1.0.jar /usr/local/ahimsa/
+sudo touch /usr/local/bin/ahimsa
+sudo echo "java -jar /usr/local/ahimsa/ahimsa-lang-1.0.jar" > /usr/local/bin/ahimsa
+sudo chmod +x /usr/local/bin/ahimsa
